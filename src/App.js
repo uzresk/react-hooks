2be2197a50833b28d23480b0b751c246545a62f3
uzresk <uzr.esk@gmail.com>
2020-01-05 18:09:54 +0900

    my first React Hook useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
-  return (
-    <div>
-      This is a template for React app.
-    </div>
-  );
-}
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    const reset = () => setCount(0);
+    const double = () => setCount(count * 2);
+
+    const increment2 = () => setCount(previousCount => previousCount + 1);
+    const decrement2 = () => setCount(previousCount => previousCount - 1);
+    const reset2 = () => setCount(previousCount => previousCount = 0);
+    const double2 = () => setCount(previousCount => previousCount * 2);
+    const divideThree2 = () => setCount(previousCount =>
+        previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+    return (
+        <>
+            <div>count: {count}</div>
+            <div>
+                {/*直接Countを編集*/}
+                <button onClick={increment}>+1</button>
+                <button onClick={decrement}>-1</button>
+                <button onClick={reset}>Reset</button>
+                <button onClick={double}>x2</button>
+            </div>
+            <div>
+                {/*関数渡し*/}
+                <button onClick={increment2}>+1</button>
+                <button onClick={decrement2}>-1</button>
+                <button onClick={reset2}>Reset2</button>
+                <button onClick={double2}>×2</button>
+                <button onClick={divideThree2}>3の倍数の時だけ3で割る</button>
+            </div>
+        </>
+    );
+};
 
 export default App;
