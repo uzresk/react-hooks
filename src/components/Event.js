92a79dfc1e87275d156c28b64e45cf90d5564d0c
uzresk <uzr.esk@gmail.com>
2020-01-10 15:30:59 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,10 @@
 import React, {useContext} from "react";
-import {DELETE_EVENT} from '../actions'
+import {
+    DELETE_EVENT,
+    ADD_OPERATION_LOG
+} from '../actions'
 import AppContext from "../contexts/AppContext";
+import {timeCurrentIso8601} from "../utils";
 
 // 一覧の要素を表示するためのComponent
 const Event = ({event}) => {
@@ -8,7 +12,14 @@ const Event = ({event}) => {
     const id = event.id;
     const handleClickDeleteButton = () => {
         const result = window.confirm(`イベント(id=${id})を本当に削除してもよいですか？`);
-        if (result) dispatch({type: DELETE_EVENT, id});
+        if (result) {
+            dispatch({type: DELETE_EVENT, id});
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: 'イベント(id=${id})を削除しました。',
+                operatedAt: timeCurrentIso8601()
+            })
+        }
     };
     return (
         <tr>
